d2d0ebb909cd0e3cede2f9bb4a935bb4f542ca81
hanshaolin <707144471@qq.com>
2018-01-14 16:56:49 +0800

    ...

--- WhereDreamStarts/ScrollViewCell.m
+++ WhereDreamStarts/ScrollViewCell.m
@@ -25,7 +25,10 @@
         _scrollView.pageDotColor=[UIColor redColor];
         [self.contentView addSubview:_scrollView];
         _scrollView.sd_layout.leftSpaceToView(self.contentView, 0).topSpaceToView(self.contentView, 0).rightSpaceToView(self.contentView, 0).bottomSpaceToView(self.contentView, 0);
-        _scrollView.imageURLStringsGroup=@[@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515925117258&di=35a3ae431a5031e88bf540d65f30dde6&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2Fuploadfile%2F2012%2F0712%2F20120712021519751.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515925150985&di=c4c9971821edefc6bba6c53404ffe2dd&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F3%2F544a2133b3049.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515925196630&di=d77562b6c7d2c122d0548267ac7ab4e5&imgtype=0&src=http%3A%2F%2Fpic35.photophoto.cn%2F20150507%2F0035035968823220_b.jpg"];
+        _scrollView.imageURLStringsGroup=@[
+                                           @"http://bpic.ooopic.com/16/47/19/16471952-c9e6fb16489ffa947b3985181f7c64ff.jpg",
+                                           @"http://img01.taopic.com/171117/267857-1G11FKI075.jpg",
+                                           @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515925196630&di=d77562b6c7d2c122d0548267ac7ab4e5&imgtype=0&src=http%3A%2F%2Fpic35.photophoto.cn%2F20150507%2F0035035968823220_b.jpg"];
     }
     return self;
 }
